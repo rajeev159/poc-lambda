87446a76dc0af69d975e1dfac05f59a072ab1a4c
Rajeev Kumar Ranjan <rajeev10.1985@rediffmail.com>
2020-01-08 12:27:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Rajeev!'),
+        body: JSON.stringify('Hello from   Rajeev!'),
     };
     return response;
 };
