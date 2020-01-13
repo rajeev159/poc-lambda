948bd20dfd87b75653b2d477c879393f350aa53e
Rajeevkumar Ranjan (Cognizant) <RajeevKumar.Ranjan@cognizant.com>
2020-01-13 10:19:25 +0530

    text changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('My name is rajeev'),
+        body: JSON.stringify('My name is Yoyo'),
     };
     return response;
 };
